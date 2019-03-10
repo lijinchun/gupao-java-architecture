4a73fdca12e6dedef6833b9ce96f38d841b0e2b5
lijinchun <13788973480@163.com>
2019-03-10 20:45:58 +0800

    gupao init

---
+++ src/main/java/com/gupao/vip2019/archiiecture/pattern/PatternApplication.java
@@ -0,0 +1,16 @@
+package com.gupao.vip2019.archiiecture.pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 李金春
+ */
+@SpringBootApplication
+public class PatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternApplication.class, args);
+    }
+
+}
